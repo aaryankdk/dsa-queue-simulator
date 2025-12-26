98c1f92f9c4aa3d58c71d549675332e7666b76cb
Aaryan Khadka <aaryankhadka25@gmail.com>
2025-12-26 14:12:40 +0545

    Initialized simulator.py skeleton

--- simulator.py
+++ simulator.py
@@ -1,1 +1,53 @@
 # Simulator: Main Program for Traffic Simulation
+
+import threading
+from queue import Queue
+
+class Vehicle:
+    def __init__(self, vehicle_id, road, lane):
+        self.vehicle_id = vehicle_id
+        self.road = road
+        self.lane = lane
+
+class FileReaderThread(threading.Thread): # Thread to read vehicle.data
+    def __init__(self, filename, queue):
+        super().__init__() 
+        self.filename = "vehicle.data"
+        self.queue = queue
+        self.daemon = True # Kills thread when main program ends1
+
+    def run(self):
+        print("File reader placeholder")
+        pass
+
+class TrafficLight:
+    def __init__(self, road):
+        self.road = road
+        self.state = 'RED'
+
+class TrafficSimulator: # Main
+    def __init__(self):
+        self.queue = {
+            'A': {1: Queue(), 2: Queue(), 3: Queue()},
+            'B': {1: Queue(), 2: Queue(), 3: Queue()},
+            'C': {1: Queue(), 2: Queue(), 3: Queue()},
+            'D': {1: Queue(), 2: Queue(), 3: Queue()}
+        }
+
+        self.traffic_lights = {
+            'A': TrafficLight('A'),
+            'B': TrafficLight('B'),
+            'C': TrafficLight('C'),
+            'D': TrafficLight('D')
+        }
+
+        def run(self):
+            print("Traffic simulator placeholder")
+            pass
+
+def main():
+    simulator = TrafficSimulator()
+    simulator.run()
+
+if __name__ == "__main__":
+    main()
